ed39a3426404aac4c4a286b9e3ba1011620370df
GT <gtalekar@infracloud.io>
2020-09-23 14:24:55 +0530

    corrected path

--- environments-ui/merge-json.sh
+++ environments-ui/merge-json.sh
@@ -13,4 +13,4 @@ done
 sed '$s/,$//' temp.json > ./src/resources/environments.json
 echo "]" >> ./src/resources/environments.json
 rm -rf temp.json
-npm i && npm run build
+cd environments-ui && npm i && npm run build

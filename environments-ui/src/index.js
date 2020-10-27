e2bf1dfb1db9b0ee96bb45185155a487b543d393
Girish Talekar <talekar.g@gmail.com>
2020-10-27 23:01:40 +0530

    Added ui for fission environments (#2)
    Co-authored-by: GT <gtalekar@infracloud.io>
    Co-authored-by: sahil-lakhwani <sahilakhwani@gmail.com>

---
+++ environments-ui/src/index.js
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import ReactDOM from "react-dom";
+import Environments from './Environments'
+
+const EnvApp = memo(props => {
+  return (
+    <Environments />
+  );
+});
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<EnvApp />, rootElement);
+
+module.hot.accept();

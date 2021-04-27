77e5270b3539aa9c7fa95eff31aa2763d985d7a6
Harsh Thakur <harshthakur9030@gmail.com>
2021-04-27 12:06:27 +0000

    Websocket nodejs (#49)
    Added support for Websocket in the NodeJS environment.

---
+++ nodejs/examples/broadcast.js
@@ -0,0 +1,9 @@
+module.exports = async function(ws, clients) {
+   
+      ws.on('message', function incoming(data) {
+          clients.forEach(function each(client) {
+              client.send(data);
+          });
+        });
+    });
+}

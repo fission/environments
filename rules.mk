301a29dafdd9b76e3385075724b2253282560920
Sanket Sudake <sanketsudake@gmail.com>
2021-06-11 18:42:00 +0530

    Add complete docker tag for reference
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- rules.mk
+++ rules.mk
@@ -7,7 +7,7 @@ TAG ?= dev
 DOCKER_FLAGS ?= --push --progress plain
 
 %-img:
-	@echo === Building image $(subst -img,,$@) using context $(CURDIR) and dockerfile $<
+	@echo === Building image $(REPO)/$(subst -img,,$@):$(TAG) using context $(CURDIR) and dockerfile $<
 	docker buildx build --platform=$(PLATFORMS) -t $(REPO)/$(subst -img,,$@):$(TAG) $(DOCKER_FLAGS) -f $< .
 
 %-builder:

5e97609b6dc8f015432065148fd60f5a76cc55b6
Suramya <shah.suramya@gmail.com>
2021-06-20 12:23:47 +0530

    add release workflow

---
+++ hack/release.sh
@@ -0,0 +1,26 @@
+path=$1
+filter=$2
+            cd $path
+            if [[ $path == "nodejs" ]]; then path="node"; fi
+            if [[ $path == "php7" ]]; then path="php"; fi          
+            if [[ "$filter" == *"builder"* ]]; then
+                cd builder
+                if [[ "$filter" == *"@"* ]]; then
+                  ver=$(echo "$filter"| cut -d '@' -f2 );
+                  version=$(cat version-$ver);
+                  make $path-builder-$ver-img TAG="$version";
+                else
+                   version=$(cat version); 
+                   make $path-builder-img TAG="$version";                                         
+                fi
+
+            else
+              if [[ "$filter" == *"@"* ]]; then
+                  ver=$(echo "$filter"| cut -d '@' -f2 );
+                  version=$(cat version-$ver);
+                  make $path-env-$ver-img TAG="$version"
+                else
+                   version=$(cat version);
+                  make $path-env-img TAG="$version" ;                   
+                fi                       
+            fi

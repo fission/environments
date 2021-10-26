f73d2035ccdec2b6b47de37b57460e6d3fda2458
Sanket Sudake <sanketsudake@gmail.com>
2021-10-26 17:36:32 +0530

    Fix local test cleanup
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- nodejs/test/local_test.sh
+++ nodejs/test/local_test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO placeholder until we have better tests :)
 
@@ -14,9 +14,11 @@ fi
 
 echo "-- Starting server"
 node $DIR/../server.js --codepath $DIR/test.js --port 8888 &
+SERVER_PID=$!
 cleanup() {
     echo "-- Cleanup"
-    kill %1
+    echo "Killing process $SERVER_PID"
+    kill $SERVER_PID
 }
 trap cleanup EXIT
 sleep 2
@@ -25,12 +27,22 @@ echo "-- Specializing"
 curl -f -X POST http://localhost:8888/specialize
 
 echo "-- Running user function"
+echo "-- GET request"
 curl -f -X GET http://localhost:8888
+echo "-- POST request"
 curl -f -X POST http://localhost:8888
+echo "-- PUT request"
 curl -f -X PUT http://localhost:8888
+echo "-- DELETE request"
 curl -f -X DELETE http://localhost:8888
+echo "-- TRACE request"
 curl -f -X TRACE http://localhost:8888
+echo "-- OPTIONS request"
 curl -f -X OPTIONS http://localhost:8888
-
+echo "-- HEAD request"
 # -I causes curl to make a HEAD request.
 curl -f -I http://localhost:8888
+echo "-- Done running jobs"
+
+echo "-- Background jobs"
+jobs

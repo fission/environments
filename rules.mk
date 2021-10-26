1a683754fe2dfceb8cee358858a677d52dac716e
Sanket Sudake <sanketsudake@gmail.com>
2021-10-26 18:17:29 +0530

    Update environment readme
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- rules.mk
+++ rules.mk
@@ -10,43 +10,7 @@ SKAFFOLD_PROFILE ?= kind
 
 %-img:
 	@echo === Building image $(REPO)/$(subst -img,,$@):$(TAG) using context $(CURDIR) and dockerfile $<
-	docker buildx build $($@-buildargs) --platform=$(PLATFORMS) -t $(REPO)/$(subst -img,,$@):$(TAG) $(DOCKER_FLAGS) -f $< .
+	echo docker buildx build $($@-buildargs) --platform=$(PLATFORMS) -t $(REPO)/$(subst -img,,$@):$(TAG) $(DOCKER_FLAGS) -f $< .
 
 %-builder:
 	cd builder/ && $(MAKE)
-
-install-skaffold:
-	@echo === Installing skaffold
-	curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
-    sudo install skaffold /usr/local/bin/ && \
-    skaffold version
-
-install-fission-cli:
-	@echo === Installing fission cli
-	curl -Lo fission https://github.com/fission/fission/releases/download/$(FISSION_VERSION)/fission-$(FISSION_VERSION)-linux-amd64 && \
-	chmod +x fission && \
-	sudo mv fission /usr/local/bin/
-
-create-crds:
-	@echo === Creating fission crds
-	kubectl create -k "github.com/fission/fission/crds/v1?ref=$(FISSION_VERSION)"
-
-verify-kind-cluster:
-	@echo === Verifying kind cluster
-	kubectl cluster-info --context kind-kind
-	kubectl get nodes
-
-skaffold-run:
-	@echo === Running skaffold
-	skaffold run -p $(SKAFFOLD_PROFILE) --tag latest
-
-go-test-images:
-	kind load docker-image go-env
-	kind load docker-image go-builder
-
-nodejs-test-images:
-	kind load docker-image node-env
-	kind load docker-image node-builder
-
-router-port-forward:
-	kubectl port-forward svc/router 8888:80 -nfission &

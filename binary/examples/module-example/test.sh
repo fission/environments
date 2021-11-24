832f40fc4fc7fa49ef91325caa6906ea0cd7eabb
Ankit <ankitc226@gmail.com>
2021-11-25 03:17:02 +0530

    Added test file

---
+++ binary/examples/module-example/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+num1=0
+num2=1
+
+for ((i=0;i<10;i++))
+do
+    echo -n "$num1"
+    sum=$((num1+num2))
+    num1=$num2
+    num2=$sum
+done
---
+++ binary/test/test_binary_env.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -euo pipefail
+ROOT=$(dirname $0)/../..
+source $ROOT/test_utils/utils.sh
+
+TEST_ID=$(generate_test_id)
+echo "TEST_ID = $TEST_ID"
+
+tmp_dir="/tmp/test-$TEST_ID"
+mkdir -p $tmp_dir
+
+cleanup() {
+    echo "cleanup"
+    clean_resource_by_id $TEST_ID
+    rm -rf $tmp_dir
+}
+
+if [ -z "${TEST_NOCLEANUP:-}" ]; then
+    trap cleanup EXIT
+else
+    log "TEST_NOCLEANUP is set; not cleaning up test artifacts afterwards."
+fi
+
+env=binary-$TEST_ID
+fn_poolmgr=binary-poolmgr-$TEST_ID
+fn_nd=binary-nd-$TEST_ID
+
+cd $ROOT/binary/examples
+
+export BINARY_BUILDER_IMAGE=binary-builder
+export BINARY_RUNTIME_IMAGE=binary-env
+
+log "Creating environment for Fission binary"
+fission env create --name $env --image $BINARY_RUNTIME_IMAGE --builder $BINARY_BUILDER_IMAGE --period 5
+
+timeout 90 bash -c "wait_for_builder $env"
+
+pkgName=$(generate_test_id)
+fission package create --name $pkgName --src hello.go --env $env
+
+# wait for build to finish at most 90s
+timeout 90 bash -c "waitBuild $pkgName"
+
+log "Creating pool manager & new deployment function for fission binary"
+fission fn create --name $fn_poolmgr --env $env --pkg $pkgName --entrypoint Handler
+fission fn create --name $fn_nd      --env $env --pkg $pkgName --entrypoint Handler --executortype newdeploy
+
+log "Creating route for new deployment function"
+fission route create --name $fn_poolmgr --function $fn_poolmgr --url /$fn_poolmgr --method GET
+fission route create --name $fn_nd --function $fn_nd --url /$fn_nd --method GET
+
+log "Waiting for router & pools to catch up"
+sleep 5
+
+log "Testing pool manager function"
+timeout 60 bash -c "test_fn $fn_poolmgr 'Hello'"
+
+log "Testing new deployment function"
+timeout 60 bash -c "test_fn $fn_nd 'Hello'"
+
+# Create zip file without top level directory (module-example)
+cd module-example && zip -0 -r $tmp_dir/module.zip * -x "*README*"
+
+pkgName=$(generate_test_id)
+fission package create --name $pkgName --src $tmp_dir/module.zip --env $env
+
+# wait for build to finish at most 90s
+timeout 90 bash -c "waitBuild $pkgName"
+
+log "Update function package"
+fission fn update --name $fn_poolmgr --pkg $pkgName
+fission fn update --name $fn_nd --pkg $pkgName
+
+log "Waiting for router & pools to catch up"
+sleep 5
+
+log "Testing pool manager function with new package"
+timeout 60 bash -c "test_fn $fn_poolmgr 'Vendor'"
+
+log "Testing new deployment function with new package"
+timeout 60 bash -c "test_fn $fn_nd 'Vendor'"
+
+log "Test PASSED"
+

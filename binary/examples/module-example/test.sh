15a9bd4636df1140cef8ae68667b9be871cd0b1b
Sanket Sudake <sanketsudake@gmail.com>
2021-12-03 13:34:30 +0530

    Fix module-example script
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- binary/examples/module-example/test.sh
+++ binary/examples/module-example/test.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 num1=0
 num2=1
 
-for ((i=0;i<10;i++))
+i=0
+while [ "$i" -le 10 ]
 do
-    echo -n "$num1"
+    echo "$num1 "
     sum=$((num1+num2))
     num1=$num2
     num2=$sum
+    i=$(( i + 1 ))
 done
 echo "Modules are awesome!"

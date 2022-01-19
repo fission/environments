b0cade98674d77a937f90894b0cb42c81764db5e
Sanket Sudake <sanketsudake@gmail.com>
2022-01-19 11:35:46 +0530

    Restore python environment state after container restart due to OOM (#213)
    * Capture exit signal with python environment
    * Restore python local tests
    * Add pkill command back
    * Store specialization info if env server restarts
    * Check if any stray python process
    * Make ports configurable
    * make runtime port int
    * Update python environment version to 1.33.1
    * Update environments-UI JSON
    
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- python/server.py
+++ python/server.py
@@ -2,7 +2,9 @@
 import importlib
 import logging
 import os
+import signal
 import sys
+import json
 
 from flask import Flask, request, abort, g
 from gevent.pywsgi import WSGIServer
@@ -10,16 +12,14 @@ import bjoern
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-
 IS_PY2 = (sys.version_info.major == 2)
 SENTRY_DSN = os.environ.get('SENTRY_DSN', None)
 SENTRY_RELEASE = os.environ.get('SENTRY_RELEASE', None)
+USERFUNCVOL = os.environ.get("USERFUNCVOL", "/userfunc")
+RUNTIME_PORT = int(os.environ.get("RUNTIME_PORT", "8888"))
 
 if SENTRY_DSN:
-    params = {
-        'dsn': SENTRY_DSN,
-        'integrations': [FlaskIntegration()]
-    }
+    params = {'dsn': SENTRY_DSN, 'integrations': [FlaskIntegration()]}
     if SENTRY_RELEASE:
         params['release'] = SENTRY_RELEASE
     sentry_sdk.init(**params)
@@ -32,6 +32,33 @@ def import_src(path):
     else:
         # the imp module is deprecated in Python3. use importlib instead.
         return importlib.machinery.SourceFileLoader('mod', path).load_module()
+
+
+def store_specialize_info(state):
+    json.dump(state, open(os.path.join(USERFUNCVOL, "state.json"), "w"))
+
+
+def check_specialize_info_exists():
+    return os.path.exists(os.path.join(USERFUNCVOL, "state.json"))
+
+
+def read_specialize_info():
+    return json.load(open(os.path.join(USERFUNCVOL, "state.json")))
+
+
+def remove_specialize_info():
+    os.remove(os.path.join(USERFUNCVOL, "state.json"))
+
+
+class SignalExit(SystemExit):
+    def __init__(self, signo, exccode=1):
+        super(SignalExit, self).__init__(exccode)
+        self.signo = signo
+
+
+def register_signal_handlers(signal_handler=signal.SIG_DFL):
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
 class FuncApp(Flask):
@@ -50,9 +77,15 @@ class FuncApp(Flask):
         #
         self.root.setLevel(loglevel)
         self.ch.setLevel(loglevel)
-        self.ch.setFormatter(logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s'))
+        self.ch.setFormatter(
+            logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         self.logger.addHandler(self.ch)
+
+        if check_specialize_info_exists():
+            self.logger.info('Found state.json')
+            specialize_info = read_specialize_info()
+            self.userfunc = self._load_v2(specialize_info)
+            self.logger.info('Loaded user function {}'.format(specialize_info))
 
         #
         # Register the routers
@@ -66,53 +99,19 @@ class FuncApp(Flask):
 
         @self.route('/v2/specialize', methods=['POST'])
         def loadv2():
-            body = request.get_json()
-            filepath = body['filepath']
-            handler = body['functionName']
-            self.logger.info(
-                '/v2/specialize called with  filepath = "{}"   handler = "{}"'.format(filepath, handler))
-
-            # handler looks like `path.to.module.function`
-            parts = handler.rsplit(".", 1)
-            if len(handler) == 0:
-                # default to main.main if entrypoint wasn't provided
-                moduleName = 'main'
-                funcName = 'main'
-            elif len(parts) == 1:
-                moduleName = 'main'
-                funcName = parts[0]
-            else:
-                moduleName = parts[0]
-                funcName = parts[1]
-            self.logger.debug(
-                'moduleName = "{}"    funcName = "{}"'.format(moduleName, funcName))
-
-            # check whether the destination is a directory or a file
-            if os.path.isdir(filepath):
-                # add package directory path into module search path
-                sys.path.append(filepath)
-
-                self.logger.debug('__package__ = "{}"'.format(__package__))
-                if __package__:
-                    mod = importlib.import_module(moduleName, __package__)
-                else:
-                    mod = importlib.import_module(moduleName)
-
-            else:
-                # load source from destination python file
-                mod = import_src(filepath)
-
-            # load user function from module
-            self.userfunc = getattr(mod, funcName)
-
+            specialize_info = request.get_json()
+            if check_specialize_info_exists():
+                self.logger.warning("Found state.json, overwriting")
+            self.userfunc = self._load_v2(specialize_info)
+            store_specialize_info(specialize_info)
             return ""
 
         @self.route('/healthz', methods=['GET'])
         def healthz():
             return "", 200
 
-        @self.route('/', methods=['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS',
-                                  'DELETE'])
+        @self.route(
+            '/', methods=['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS', 'DELETE'])
         def f():
             if self.userfunc is None:
                 print("Generic container: no requests supported")
@@ -129,17 +128,71 @@ class FuncApp(Flask):
 
             return self.userfunc()
 
+    def _load_v2(self, specialize_info):
+        filepath = specialize_info['filepath']
+        handler = specialize_info['functionName']
+        self.logger.info(
+            'specialize called with  filepath = "{}"   handler = "{}"'.format(
+                filepath, handler))
+        # handler looks like `path.to.module.function`
+        parts = handler.rsplit(".", 1)
+        if len(handler) == 0:
+            # default to main.main if entrypoint wasn't provided
+            moduleName = 'main'
+            funcName = 'main'
+        elif len(parts) == 1:
+            moduleName = 'main'
+            funcName = parts[0]
+        else:
+            moduleName = parts[0]
+            funcName = parts[1]
+        self.logger.debug('moduleName = "{}"    funcName = "{}"'.format(
+            moduleName, funcName))
 
-app = FuncApp(__name__, logging.DEBUG)
+        # check whether the destination is a directory or a file
+        if os.path.isdir(filepath):
+            # add package directory path into module search path
+            sys.path.append(filepath)
 
-#
-# TODO: this starts the built-in server, which isn't the most
-# efficient.  We should use something better.
-#
-if os.environ.get("WSGI_FRAMEWORK") == "GEVENT":
-    app.logger.info("Starting gevent based server")
-    svc = WSGIServer(('0.0.0.0', 8888), app)
-    svc.serve_forever()
-else:
-    app.logger.info("Starting bjoern based server")
-    bjoern.run(app, '0.0.0.0', 8888, reuse_port=True)
+            self.logger.debug('__package__ = "{}"'.format(__package__))
+            if __package__:
+                mod = importlib.import_module(moduleName, __package__)
+            else:
+                mod = importlib.import_module(moduleName)
+
+        else:
+            # load source from destination python file
+            mod = import_src(filepath)
+
+        # load user function from module
+        return getattr(mod, funcName)
+
+    def signal_handler(self, signalnum, frame):
+        self.logger.info('Received signal {}'.format(
+            signal.strsignal(signalnum)))
+        if check_specialize_info_exists():
+            self.logger.info('Found state.json, removing')
+            remove_specialize_info()
+        signal.signal(signalnum, signal.SIG_DFL)
+        raise SignalExit(signalnum)
+
+
+def main():
+    register_signal_handlers()
+    app = FuncApp(__name__, logging.DEBUG)
+    register_signal_handlers(app.signal_handler)
+
+    #
+    # TODO: this starts the built-in server, which isn't the most
+    # efficient.  We should use something better.
+    #
+    if os.environ.get("WSGI_FRAMEWORK") == "GEVENT":
+        app.logger.info("Starting gevent based server")
+        svc = WSGIServer(('0.0.0.0', RUNTIME_PORT), app)
+        svc.serve_forever()
+    else:
+        app.logger.info("Starting bjoern based server")
+        bjoern.run(app, '0.0.0.0', RUNTIME_PORT, reuse_port=True)
+
+
+main()
--- python/tests/local_test.sh
+++ python/tests/local_test.sh
@@ -19,33 +19,35 @@ SERVER_PID=$!
 cleanup() {
     echo "-- Cleanup"
     echo "Killing process $SERVER_PID"
+    pkill -f '.*server.py'
     kill $SERVER_PID
     deactivate
     rm -r test_env
+    ps -ef | grep python3 | grep -v grep
 }
 trap cleanup EXIT
 sleep 5
 
 echo "--Healthz"
-curl -f -X GET http://localhost:8888/healthz
+curl -f -X GET http://localhost:$RUNTIME_PORT/healthz
 
 echo "-- Specializing"
-curl -f -XPOST http://localhost:8888/v2/specialize -H 'Content-Type: application/json' -d '{"filepath": "./examples/hello.py", "functionName": "main"}'
+curl -f -XPOST http://localhost:$RUNTIME_PORT/v2/specialize -H 'Content-Type: application/json' -d '{"filepath": "./examples/hello.py", "functionName": "main"}'
 
 echo "-- Running user function"
 echo "-- GET request"
-curl -f -X GET http://localhost:8888
+curl -f -X GET http://localhost:$RUNTIME_PORT
 echo "-- POST request"
-curl -f -X POST http://localhost:8888
+curl -f -X POST http://localhost:$RUNTIME_PORT
 echo "-- PUT request"
-curl -f -X PUT http://localhost:8888
+curl -f -X PUT http://localhost:$RUNTIME_PORT
 echo "-- DELETE request"
-curl -f -X DELETE http://localhost:8888
+curl -f -X DELETE http://localhost:$RUNTIME_PORT
 echo "-- OPTIONS request"
-curl -f -X OPTIONS http://localhost:8888
+curl -f -X OPTIONS http://localhost:$RUNTIME_PORT
 echo "-- HEAD request"
 # -I causes curl to make a HEAD request.
-curl -f -I http://localhost:8888
+curl -f -I http://localhost:$RUNTIME_PORT
 echo "-- Done running jobs"
 
 echo "-- Background jobs"

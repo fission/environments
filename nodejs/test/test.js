b11017640361117e82889fcef334209d2b6e45b1
Sanket Sudake <sanketsudake@gmail.com>
2025-08-12 18:03:14 +0530

    ESM and commonJS trying out in existing env
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- nodejs/test/test.js
+++ nodejs/test/test.js
@@ -1,3 +1,5 @@
+// Both should work with env var set.
+
 module.exports = async function (context) {
     console.log("headers=", JSON.stringify(context.request.headers));
     console.log("body=", JSON.stringify(context.request.body));
@@ -7,3 +9,17 @@ module.exports = async function (context) {
         body: "Hello, world !\n"
     };
 }
+// export default async function (context) {
+//     console.log("ESM headers=", JSON.stringify(context.request.headers));
+//     console.log("ESM body=", JSON.stringify(context.request.body));
+//     console.log("ESM method=", context.request.method);
+
+//     return {
+//         status: 200,
+//         body: "Hello, world !\n",
+//         headers: {
+//             "X-Module-Type": "ESM",
+//             "X-Node-Version": process.version
+//         }
+//     };
+// }
